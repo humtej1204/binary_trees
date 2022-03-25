823300af5b6dd220fc00cbdd3e15a97b15be94dd
Humberto Tejada <humtej1204@gmail.com>
2022-03-24 23:24:19 -0500

    Correcting Betty Style

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,15 +34,15 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t l = 0, r = 0;
+	size_t l = 0, r = 0;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        if (tree->left != NULL)
-                l = (1 + binary_tree_height(tree->left));
-        if (tree->right != NULL)
-                r = (1 + binary_tree_height(tree->right));
+	if (tree->left != NULL)
+		l = (1 + binary_tree_height(tree->left));
+	if (tree->right != NULL)
+		r = (1 + binary_tree_height(tree->right));
 
-        return (l > r ? l : r);
+	return (l > r ? l : r);
 }
