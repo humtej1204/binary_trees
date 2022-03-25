7cb6d3aa83df3ea0fc9e183ef30942e36a78449f
Humberto Tejada <humtej1204@gmail.com>
2022-03-24 21:00:05 -0500

    Correcting Betty Style

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -14,7 +14,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 
 	if (tree->left != NULL)
 		binary_tree_inorder(tree->left, func);
-	
+
 	func(tree->n);
 
 	if (tree->right != NULL)
